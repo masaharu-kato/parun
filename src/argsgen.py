8ca386b9ac2749024a456f5dbe214c2c141e2b5b
Masaharu Kato (Desktop) <fudai.mk@gmail.com>
2019-11-22 00:39:58 +0900

    modified Union and Product

--- src/argsgen.py
+++ src/argsgen.py
@@ -1,6 +1,6 @@
 import itertools
 
-from typing import Any, Dict, Iterable, Iterator, List, Optional, Union as UnionType
+from typing import Any, Dict, Iterable, Iterator, List, Optional, Tuple, Union as UnionType
 ArgVal = UnionType[str, Iterable[str]] # Optional[Union[int, float, str]]
 IArgVal = UnionType[ArgVal, List[ArgVal]]
 
@@ -17,24 +17,25 @@ class Args:
 
 
     @staticmethod
-    def keyvalue_to_cmd_args(keyvalue:Tuple[ArgName, ArgVal]) -> Iterator[str]:
+    def keyvalue_to_cmd_args(keyvalue:Tuple[str, ArgVal]) -> Iterator[str]:
         key, v = keyvalue
         yield key
         if v is not None:
             if hasattr(v, '__iter__'):
                 yield from v
-            else:
+            else: 
                 yield v
 
 
 
-class Union():
+class Union:
     def __init__(self, *vals):
         self.vals = vals
 
 
-    def __iter__(self):
-        return self.vals
+    def __iter__(self) -> Iterator:
+        yield from self.vals
+
 
 
 class Product:
@@ -42,25 +43,28 @@ class Product:
         self.vals = vals
 
 
-    def __iter__(self):
-        #TODO: Correct Implementation
-        return itertools.product(*self.vals)
+    def __iter__(self) -> Iterator:
+        yield from itertools.product(
+            *(v if hasattr(v, '__iter__') else [v] for v in self.vals)
+        )
 
 
 
-class Format():
+class Format:
     def __init__(self, text:str):
         self.text = text
 
 
-    def format(self, args:Args):
-        # TODO: Corresponds to positional arguments
-        return self.text.format(*args.kwargs)
+    def format(self, args:Args) -> str:
+        return self.text.format(
+            **{'$'+str(i):v for i, v in enumerate(args.pargs, 1)},
+            **args.kwargs
+        )
 
 
 
 class ArgsPattern:
-    def __init__(self, iargs:Dict[ArgName, IArgVal]):
+    def __init__(self, iargs:Dict[str, IArgVal]):
         self.iargs = iargs
 
 
@@ -72,7 +76,7 @@ class ArgsPattern:
 
 
     @staticmethod
-    def iargval_to_argval(v:ArgValueEx) -> Iterator[ArgVal]:
+    def iargval_to_argval(v) -> Iterator[ArgVal]:
         if hasattr(v, '__iter__'):
             yield from v
         else:
@@ -81,5 +85,5 @@ class ArgsPattern:
 
     @staticmethod
     def dict_product_with_kv(keys:Iterable, values:Iterable) -> Iterator[Dict]:
-        return (dict(zip(keys, values)) for values in itertools.product(values))
+        return ({**zip(keys, values)} for values in itertools.product(values))
 
