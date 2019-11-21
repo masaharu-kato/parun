7f7be7116affb4b6b5a3aa1c66ccc59a28f019be
Masaharu Kato (lab-CKG) <saa01068@gmail.com>
2019-11-21 21:28:46 +0900

    add quiet option

--- src/executor.py
+++ src/executor.py
@@ -1,8 +1,8 @@
 import subprocess
 
-def execute(commands:list):
+def execute(commands:list, *, quiet:bool=False):
     for command in commands:
-        print(' '.join(command))
+        if not quiet: print(' '.join(command))
         p = subprocess.Popen(command, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
         for line in iter(p.stdout.readline,b''):
             print(line.decode("utf8"), end='')
--- src/main.py
+++ src/main.py
@@ -9,9 +9,13 @@ def main():
     argp = argparse.ArgumentParser()
     argp.add_argument('args_path', type=str, help="Path for params.json")
     argp.add_argument('args_name', type=str, help="Args name to run")
+    argp.add_argument('-q', '--quiet', action='store_true', help='Do not output messages to stdout')
     args = argp.parse_args()
 
-    return executor.execute(argsgen.generate_commands_from_setting_file(args.args_path, args.args_name))
+    return executor.execute(
+        argsgen.generate_commands_from_setting_file(args.args_path, args.args_name),
+        quiet=args.quiet
+    )
 
 
 if __name__ == '__main__':
