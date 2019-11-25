5b61cd607845fb4433dc18bb4b8f3cc362d3a215
Masaharu Kato (lab-CKG) <saa01068@gmail.com>
2019-11-25 19:24:53 +0900

    implemented argsgen (WIP)

--- src/argsgen.py
+++ src/argsgen.py
@@ -1,67 +1,98 @@
 import itertools
-
-from typing import Any, Dict, Iterable, Iterator, List, Optional, Tuple, Union as UnionType
-ArgVal = UnionType[str, Iterable[str]] # Optional[Union[int, float, str]]
-IArgVal = UnionType[ArgVal, List[ArgVal]]
-
-
-class Args:
-    def __init__(self, pargs:list, kwargs:dict):
-        self.pargs = pargs
-        self.kwargs = kwargs
-        self.args_for_format = {
-            **{'$'+str(i):v for i, v in enumerate(self.pargs, 1)},
-            **self.kwargs
-        }
-
-
-    def cmdargs(self) -> Iterator[str]:
-        yield from map(str, self.raw_cmdargs())
-
-
-    def raw_cmdargs(self) -> Iterator:
-        yield from self.pargs
-        yield from itertools.chain.from_iterable(map(self.keyvalue_to_cmd_args, self.kwargs.items()))
-
-
-    @staticmethod
-    def keyvalue_to_cmdargs(keyvalue:Tuple[str, ArgVal]) -> Iterator[str]:
-        key, v = keyvalue
-        yield key
-        if v is not None:
-            if isinstance(v, Iterable):
-                yield from v
-            else:
-                yield v
+import upiter
+from typing import Any, cast, Dict, Iterable, Iterator, List, Optional, Tuple, Union
 
 
 class Format:
-    def __init__(self, text:str, args:Dict[str, ArgVal]):
-        self.text = text
-        self.args = args
+    """ Formatted argument value obejct
+    """
+    def __init__(self, text:str, args:Dict[str, Any]):
+        self.text = text # format text
+        self.args = args # (Reference to) arguments
 
 
     def __str__(self) -> str:
+        """ get formatted string with arguments """
         return self.text.format(**self.args)
 
 
+# Scalar = Union[None, bool, int, float, str]
+ArgVal = Any #Union[Scalar, Iterable[Any], Format]
+Args = Dict[str, ArgVal] # (All-) Arguments dictionary
 
-class ArgsPattern:
-    def __init__(self, iargs:Dict[str, IArgVal]):
-        self.iargs = iargs
-
-
-    def args(self) -> Iterator[Dict[str, str]]:
-        return self.dict_product_with_kv(
-            self.iargs.keys(),
-            map(self.iargval_to_argval, self.iargs.values())
-        )
+_PArgs_ = '' # Key name of positional arguments in all-arguments dictionary 
 
 
-    @staticmethod
-    def iargval_to_argval(v) -> Iterator[ArgVal]:
-        if isinstance(v, Iterable):
-            yield from v
-        else:
-            yield v
+def pattern_to_cmdargs_iter(pattern) -> Iterator[Iterator[str]]:
+    """ Generate iterators of commandline arguments from pattern """
+    return map(args_to_cmdargs, pattern_to_args_iter(pattern))
 
+
+def pattern_to_args_iter(pattern) -> Iterator[Args]:
+    """ Generate iterators of argument objects from pattern """
+    return map(rawargs_to_args, pattern_to_rawargs_iter(pattern))
+
+
+def pattern_to_rawargs_iter(pattern) -> Iterator[Args]:
+    """ Generate iterators of raw argument values from pattern """
+    # TODO: Correct Implementation (when top of `pattern` is not dictionary)
+    return cast(Iterator[Args], upiter.product_union(pattern))
+
+
+def rawargs_to_args(rawargs:Args) -> Args:
+    """ Convert raw argument values to final values
+        (Create `Format` object in specified value) 
+    """
+
+    args_for_format:Args = {}
+
+    def make_value(v):
+        if isinstance(v, dict) and '__format__' in v:
+            return Format(v['__format__'], args_for_format)
+        return v
+
+    args = {k:make_value(v) for k, v in rawargs.items()}
+
+    args_for_format.update(make_args_for_format(args))
+
+    return args
+
+
+def args_to_cmdargs(args:Args) -> Iterator[str]:
+    """ Iterate final command argument strings """
+    yield from map(str, args_to_cmdargobjs(args))
+
+
+def args_to_cmdargobjs(args:Args) -> Iterator:
+    """ Iterate command arguments (as string or other object) """
+    yield from get_pargs(args)
+    yield from itertools.chain.from_iterable(keyvalue_to_cmdargs(k, v) for k, v in get_kwargs(args))
+
+
+def keyvalue_to_cmdargs(k:str, v:ArgVal) -> Iterator[str]:
+    """ Iterate key-value pair (yield key and value)
+        If value is None, yield only key
+        If value is iterable, yield from the value
+    """
+    yield k
+    if v is not None: yield from upiter.as_iterable(v)
+
+
+def make_args_for_format(args:Args) -> Args:
+    """ Make dictionary for formatting
+        Assign positional arguments as key '$(number)' in addition to keyword arguments (except Format object)
+    """
+    return {
+        **{'$'+str(i):v for i, v in enumerate(get_pargs(args), 1) if not isinstance(v, Format)},
+        **{k:v for k, v in get_kwargs(args) if not isinstance(v, Format)}
+    }
+
+
+def get_pargs(args:Args) -> Iterable[ArgVal]:
+    """ Get positional arguments from all-arguments dictionary """
+    return upiter.as_iterable(args.get(_PArgs_))
+
+
+def get_kwargs(args:Args) -> Iterator[Tuple[str, ArgVal]]:
+    """ Get keyword arguments from all-arguments dictionary """
+    return ((k, v) for k, v in args.items() if k != _PArgs_)
---
+++ src/test_argsgen.py
@@ -0,0 +1,27 @@
+import pytest #type:ignore
+import argsgen
+import json
+
+
+with open('test/res/print_args.json', mode='r') as f:
+    cfg_data = json.load(f)
+
+with open('test/res/print_args_result.json', mode='r') as f:
+    result_data = json.load(f)
+
+
+inputs = cfg_data['args']
+results = result_data['args_result']
+
+test_patterns = {name:(_input, results[name]) for name, _input in inputs.items()}
+
+
+@pytest.mark.parametrize(
+    'pattern, ans_cmdargs_list',
+    [*test_patterns.values()],
+    ids = [*test_patterns.keys()]   
+)
+def test_argsgen(pattern, ans_cmdargs_list):
+    cmdargs_iter = argsgen.pattern_to_cmdargs_iter(pattern)
+    return all([*res] == ans for res, ans in zip(cmdargs_iter, ans_cmdargs_list))
+
